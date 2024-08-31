d67101f3ea7868d277e312713020004d03a0f9b9
MboteHub <130560570+MboteHub@users.noreply.github.com>
2024-08-31 02:16:52 +0200

    little change

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -21,13 +21,14 @@ module.exports = {
         thin: '100',
         extraLight: '200',
         light: '300',
-        normal: '400',
+        normal: '400', 
         medium: '500',
         semiBold: '600',
         bold: '700',
         extraBold: '800',
         black: '900',
       },
+      
     },
   },
   plugins: [
